8d8c6d79dfd34932a0e2ef21568f44198501d619
Krisbell26 <krisbelldrnw@gmail.com>
2019-05-10 09:38:11 +0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -17,11 +17,11 @@ const path = requre('path');
 const app = express();
 
 //serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/testheroku-angular-app'));
+app.use(express.static(__dirname + '/weblanjutheroku'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/testheroku-angular-app/index.html'));
+res.sendFile(path.join(__dirname+'/weblanjutheroku/index.html'));
 });
 
 //start the app by listening on the default Heroku port
